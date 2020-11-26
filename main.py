9647c4b9f33eaba5c1ac84d6e56be6cc663a694d
Константин <kostyad2005@gmail.com>
2020-11-26 17:52:24 +0300

    FINAL

--- main.py
+++ main.py
@@ -4,25 +4,31 @@ from PyQt5.QtGui import QPainter, QColor
 
 from Ui import Ui_MainWindow
 from PyQt5.QtWidgets import QMainWindow, QApplication
-from random import randint, random
+import random
 
 
 class Dis(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.pushButton.clicked.connect(self.draw)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
 
     def paintEvent(self, event):
-        qp = QPainter()
-        qp.begin(self)
-        self.draw(qp)
-        qp.end()
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
 
     def draw(self, qp):
-        a = random.randint(1, 540)
+        a = int(random.randint(1, 400))
         qp.setBrush(QColor(255, 255, 0))
-        qp.drawEllipse(a)
+        qp.drawEllipse(100, 50, a, a)
 
 
 if __name__ == '__main__':
